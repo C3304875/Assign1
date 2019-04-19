2863ad4e8a63577895e4901dc739249983a99122
C3304875 <Lazycats2299#>
2019-04-19 20:52:24 +1000

    decryptSubstitutionCypher() now works correctly (finally). Program remains unpolished.

--- main.c
+++ main.c
@@ -10,22 +10,20 @@ void decryptRotationCypher(const char *cypherText, char *outputText, int rotatio
 void encryptSubstitutionCypher(const char *cypherText, char *outputText);
 void decryptSubstitutionCypher(const char *cypherText, char *outputText);
 
-int main(){
+int main() {
    FILE *input, *output;
    char cypherText[1024], outputText[1024];
-   int i = 0, selector, rotationAmount;
+   int i = 0, selector = 0, rotationAmount;
 
    // Initialise input & output files.
    output = fopen("Resources/output.txt", "w");
    input = fopen("Resources/input.txt", "r");
-   if (input == NULL){
+   if (input == NULL)
       perror("File Input");
-   }
 
    // Read input into char array cypherText.
-   while (!feof(input)){
+   for (i = 0; cypherText[i] != feof(input); i++){
       fscanf(input, "%c", &cypherText[i]);
-      i++;
    }
 
    printf("\n1. Rotation Cypher Encryption\n2. Rotation Cypher Decryption\n");
@@ -33,7 +31,7 @@ int main(){
    printf("\nPlease Make a Selection: \n");
    scanf("%d", &selector);
 
-   switch (selector){
+   switch (selector) {
       case 1:
          printf("Please enter rotation amount between 0 & 25: \n");
          scanf("%d", &rotationAmount);
@@ -54,67 +52,85 @@ int main(){
          printf("Please Enter a Valid Selection.\n");
          break;
    }
-   for (i = 0; outputText[i] != '\0'; i++)
+   printf("\n");
+   for (i = 0; cypherText[i] != '\0'; i++){
       printf("%c", outputText[i]);
-
+   }
 }
 
-void encryptRotationCypher(const char *cypherText, char *outputText, int rotationAmount){ // e(x) = (m + k)(mod 26)
+void encryptRotationCypher(const char *cypherText, char *outputText, int rotationAmount) { // e(x) = (m + k)(mod 26)
    int i;
 
-   for (i = 0; cypherText[i] != '\0'; i++){
-      if (cypherText[i] >= 'A' && cypherText[i] <= 'Z'){
+   for (i = 0; cypherText[i] != '\0'; i++) {
+      if (cypherText[i] >= 'A' && cypherText[i] <= 'Z') {
          outputText[i] = cypherText[i] + (rotationAmount % 26);
-         if (outputText[i] > 'Z'){
+         if (outputText[i] > 'Z')
             outputText[i] = ('A' - 1 + (outputText[i] - 'Z') % 26);
-         }
-         if (outputText[i] < 'A'){
+         if (outputText[i] < 'A')
             outputText[i] = ('Z' - 1 + (outputText[i] - 'A') % 26);
-         }
       }
-      else {
-         outputText[i] = cypherText[i]; // Skips any non-alphabet characters
-      }
+      else
+         outputText[i] = cypherText[i]; // Skips any non-alphabet character
    }
 }
 
-void decryptRotationCypher(const char *cypherText, char *outputText, int rotationAmount){ // d(c) = (c − k)(mod 26)
+void decryptRotationCypher(const char *cypherText, char *outputText, int rotationAmount) { // d(c) = (c − k)(mod 26)
    int i;
 
-   for (i = 0; cypherText[i] != '\0'; i++){
-      if (cypherText[i] >= 'A' && cypherText[i] <= 'Z'){
+   for (i = 0; cypherText[i] != '\0'; i++) {
+      if (cypherText[i] >= 'A' && cypherText[i] <= 'Z') {
          outputText[i] = cypherText[i] - (rotationAmount % 26);
-         if (outputText[i] > 'Z'){
+         if (outputText[i] > 'Z')
             outputText[i] = ('A' + 1 + (outputText[i] - 'Z') % 26);
-         }
-         if (outputText[i] < 'A'){
+         if (outputText[i] < 'A')
             outputText[i] = ('Z' + 1 + (outputText[i] - 'A') % 26);
-         }
       }
-      else {
+      else
          outputText[i] = cypherText[i]; // Skips any non-alphabet characters
-      }
    }
 }
 
-void encryptSubstitutionCypher(const char *cypherText, char *outputText){
+void encryptSubstitutionCypher(const char *cypherText, char *outputText) {
    const char sub[26] = {"QAZXSWEDCVFRTGBNHYUJMKILOP"};
-   char altered[26];
-   int position[1024];
+   int position[1024], i;
 
-   int i;
-
-   for (i = 0; cypherText[i] != '\0'; i++){
+   for (i = 0; cypherText[i] != '\0'; i++) {
       if (cypherText[i] >= 'A' && cypherText[i] <= 'Z') {
          position[i] = cypherText[i] - 65;
          outputText[i] = sub[position[i]];
       }
-      else {
+      else
          outputText[i] = cypherText[i];
-      }
    }
 }
 
-void decryptSubstitutionCypher(const char *cypherText, char *outputText){
-   printf("UNDER CONSTRUCTION");
+void decryptSubstitutionCypher(const char *cypherText, char *outputText) {
+   const char orig[26] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, sub[26] = {"QAZXSWEDCVFRTGBNHYUJMKILOP"};
+   char oldLetter, newLetter;
+
+   int i, j, k = 0;
+
+   for (i = 0; cypherText[i] != '\0'; i++) {
+      oldLetter = sub[i];
+      newLetter = orig[i];
+      if (cypherText[i] >= 'A' && cypherText[i] <= 'Z') {
+         for (j = 0; j < 100; j++) {
+            if (cypherText[i] == oldLetter) {
+               outputText[i] = newLetter;
+               printf("%d", outputText[i]);
+               break;
+            }
+            else {
+               j++;
+               oldLetter = sub[k];
+               newLetter = orig[k];
+               k++;
+               if (k > 25)
+                  k = 0;
+            }
+         }
+      }
+      else
+         outputText[i] = cypherText[i];
+   }
 }
